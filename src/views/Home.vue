f1ac1ecfa437cd5b127a2442db177ac440c1ccb1
Snufty <56336551+Snufty@users.noreply.github.com>
2019-12-05 13:28:16 +0100

    vapp

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,38 @@
 <template>
-  <div class="home">
-    <div>
-      <v-app-bar id="header" color="green" dark prominent>
-        <v-spacer></v-spacer>
+  <v-app>
+    <div class="home">
+      <div>
+        <v-app-bar id="header" color="green" dark prominent>
+          <v-spacer></v-spacer>
 
-        <v-toolbar-title id="titel">Illustreret Videnskab</v-toolbar-title>
+          <v-toolbar-title id="titel">Illustreret Videnskab</v-toolbar-title>
 
-        <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
 
-        <v-menu left bottom>
-          <template v-slot:activator="{ on }">
-            <v-btn icon v-on="on">
-              <v-icon>mdi-dots-vertical</v-icon>
-            </v-btn>
-          </template>
+          <v-menu left bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn icon v-on="on">
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </template>
 
-          <v-list>
-            <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-              <v-list-item-title>Option {{ n }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </v-app-bar>
+            <v-list>
+              <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+                <v-list-item-title>Option {{ n }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-app-bar>
+      </div>
+
+      <section id="top-section"></section>
+      <!-- Adnan -->
+      <section id="middle-section"></section>
+      <!-- Tobias -->
+      <section id="bottom-section"></section>
+      <!-- Alex -->
     </div>
-
-    <section id="top-section"></section>
-    <!-- Adnan -->
-    <section id="middle-section"></section>
-    <!-- Tobias -->
-    <section id="bottom-section"></section>
-    <!-- Alex -->
-  </div>
+  </v-app>
 </template>
 
 <script>
@@ -42,7 +44,4 @@ export default {
 </script>
 
 <style lang="scss">
-#header {
-  background-color: green;
-}
 </style>
