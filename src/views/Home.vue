8ef96c98d29ed35140f6d0dae5eaa7d0ec912b34
AdnanSB <56301144+AdnanSB@users.noreply.github.com>
2019-12-05 12:18:32 +0100

    sections

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <h1>pølser</h1>
-    <v-card class="mx-auto" max-width="344" outlined>
-      <v-list-item three-line>
-        <v-list-item-content>
-          <div class="overline mb-4">OVERLINE</div>
-          <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
-          <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
-      </v-list-item>
-
-      <v-card-actions>
-        <v-btn text>Button</v-btn>
-        <v-btn text>Button</v-btn>
-      </v-card-actions>
-    </v-card>
+    <section id="top-section"></section>
+    <section id="middle-section"></section>
+    <section id="bottom-section"></section>
   </div>
 </template>
 
