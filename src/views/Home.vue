182ba84b3b13183fac9e14cfb1fa3be570ab7fd7
Snufty <56336551+Snufty@users.noreply.github.com>
2019-12-05 12:00:58 +0100

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>pøls</h1>
+    <v-card class="mx-auto" max-width="344" outlined>
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="overline mb-4">OVERLINE</div>
+          <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
+          <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+        </v-list-item-content>
+
+        <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+      </v-list-item>
+
+      <v-card-actions>
+        <v-btn text>Button</v-btn>
+        <v-btn text>Button</v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home"
+};
 </script>
+
+<style lang="scss">
+$color: red;
+h1 {
+  color: $color;
+}
+</style>
