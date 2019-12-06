30d7581ec4f12320a44c75a193f2d6b6d6c7d7ec
Snufty <56336551+Snufty@users.noreply.github.com>
2019-12-06 10:37:05 +0100

    fuckfuckfcuk

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,9 @@
       <div>
         <v-app-bar color="deep-purple accent-4" height="120">
           <v-app-bar-nav-icon>
-              <div><img src="../assets/logo_rumaffald.svg" height="45" width="56" alt="">
-              </div>
+            <div>
+              <img src="../assets/logo_rumaffald.svg" height="45" width="56" alt />
+            </div>
           </v-app-bar-nav-icon>
 
           <v-spacer></v-spacer>
@@ -35,39 +36,12 @@
       </div>
 
       <v-container id="sec_container" fluid no gutters>
-<<<<<<< HEAD
         <v-row no gutters>
           <div id="sec_1"></div>
-=======
-        <v-row no gutters contain>
-          <div id="sec_1">
-            <div class="text-center">
-              <v-dialog v-model="dialog" width="500">
-                <template v-slot:activator="{ on }">
-                  <v-btn color="red lighten-2" dark v-on="on"></v-btn>
-                </template>
-
-                <v-card>
-                  <v-card-title class="headline grey lighten-2" primary-title>Privacy Policy</v-card-title>
-
-                  <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</v-card-text>
-
-                  <v-divider></v-divider>
-
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="primary" text @click="dialog = false">I accept</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-            </div>
-          </div>
->>>>>>> 81973435939984d602a5dc3602a60601e2853884
         </v-row>
 
-        
         <v-row no gutters>
-          <div id="sec_2"></div>
+          <div id="sec_2">github er det værste lort</div>
         </v-row>
         <v-row no gutters>
           <div id="sec_3"></div>
@@ -93,12 +67,8 @@ export default {
 #sec_1 {
   height: 100vh;
   width: 100vw;
-<<<<<<< HEAD
   background-color: red;
   background-image: url("https://svgshare.com/i/GdL.svg");
-=======
-  background-image: url("../assets/background.svg");
->>>>>>> 81973435939984d602a5dc3602a60601e2853884
   background-size: cover;
 }
 
