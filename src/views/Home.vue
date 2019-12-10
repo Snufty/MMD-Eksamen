5ee12a76650b1fc825c6bc25ee60e172b3099a22
Snufty <56336551+Snufty@users.noreply.github.com>
2019-12-10 10:44:41 +0100

    lmaooo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,46 @@
         <v-row no gutters>
           <div id="sec_1">
             <v-img src="https://svgshare.com/i/Ggy.svg" height="250" width="250" id="debris_1"></v-img>
+            <v-dialog
+              v-model="dialog2"
+              max-width="300"
+              scrollable
+              content-class="dialog2"
+              hide-overlay="true"
+            >
+              <template v-slot:activator="{ on }">
+                <v-btn icon depressed v-on="on" color="transparent" id="satelite">
+                  <v-img src="https://svgshare.com/i/GeR.svg" height="550" width="550"></v-img>
+                </v-btn>
+              </template>
 
+              <v-card>
+                <v-card-title class="headline grey lighten-2" primary-title>Asteroid</v-card-title>
+                <v-card-text>
+                  What we see here is a very beautiful Asteroid
+                  Once a upon a time this little thing went like pawww skrrr
+                </v-card-text>
+                <v-divider></v-divider>
+                <v-img
+                  id="card-img"
+                  contain
+                  src="https://media.giphy.com/media/fYexKcRTSFumjejKPe/giphy.gif"
+                  height="170"
+                  width="120"
+                ></v-img>
+                <v-spacer></v-spacer>
+                <v-divider></v-divider>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="primary" text @click="dialog2 = false">close</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </div>
+        </v-row>
+
+        <v-row no gutters>
+          <div id="sec_2">
             <v-dialog
               v-model="dialog1"
               max-width="300"
@@ -23,9 +62,12 @@
             >
               <template v-slot:activator="{ on }">
                 <v-btn icon depressed v-on="on" color="transparent">
-                  <v-avatar height="120" width="120" tile>
-                    <img src="https://image.flaticon.com/icons/svg/1684/1684370.svg" />
-                  </v-avatar>
+                  <v-img
+                    src="https://svgshare.com/i/GhY.svg"
+                    height="600"
+                    width="600"
+                    id="satelite2"
+                  ></v-img>
                 </v-btn>
               </template>
               <v-card>
@@ -47,45 +89,6 @@
                 <v-card-actions>
                   <v-spacer></v-spacer>
                   <v-btn color="primary" text @click="dialog1 = false">close</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </div>
-        </v-row>
-
-        <v-row no gutters>
-          <div id="sec_2">
-            <v-dialog
-              v-model="dialog2"
-              max-width="300"
-              scrollable
-              content-class="dialog2"
-              hide-overlay="true"
-            >
-              <template v-slot:activator="{ on }">
-                <v-btn icon depressed v-on="on" color="transparent" id="satelite">
-                  <v-img src="https://svgshare.com/i/GeR.svg" height="550" width="550"></v-img>
-                </v-btn>
-              </template>
-              <v-card>
-                <v-card-title class="headline grey lighten-2" primary-title>Asteroid</v-card-title>
-                <v-card-text>
-                  What we see here is a very beautiful Asteroid
-                  Once a upon a time this little thing went like pawww skrrr
-                </v-card-text>
-                <v-divider></v-divider>
-                <v-img
-                  id="card-img"
-                  contain
-                  src="https://media.giphy.com/media/fYexKcRTSFumjejKPe/giphy.gif"
-                  height="170"
-                  width="120"
-                ></v-img>
-                <v-spacer></v-spacer>
-                <v-divider></v-divider>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="primary" text @click="dialog2 = false">close</v-btn>
                 </v-card-actions>
               </v-card>
             </v-dialog>
@@ -175,12 +178,16 @@ export default {
 #satelite {
   position: relative;
   left: 30%;
-  top: 15%;
   transition: 8s linear;
   -webkit-animation: shake-vertical 10s cubic-bezier(0.455, 0.03, 0.515, 0.955)
     both;
   animation: shake-vertical 10s cubic-bezier(0.455, 0.03, 0.515, 0.955) both
     infinite;
+}
+
+#satelite2 {
+  transform: rotate(50deg);
+  right: 65%;
 }
 
 #debris_1 {
@@ -191,22 +198,19 @@ export default {
 
 @keyframes kurve_translate {
   0% {
-    transform: translate(-10%, 0px);
-  }
-  20% {
-    transform: translate(120%, -150px);
-  }
-  40% {
-    transform: translate(240%, -180px);
-  }
-  60% {
-    transform: translate(360%, -100px);
-  }
-  80% {
-    transform: translate(480%, 30px);
+    transform: translateX(-115%);
   }
   100% {
-    transform: translate(600%, 300px);
+    transform: translateX(600%);
+  }
+}
+
+@keyframes spin {
+  from {
+    -webkit-transform: rotate(0deg);
+  }
+  to {
+    -webkit-transform: rotate(360deg);
   }
 }
 
