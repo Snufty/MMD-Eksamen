71506ee4084af876b22446854a918ef6e52e31a6
AdnanSB <56301144+AdnanSB@users.noreply.github.com>
2019-12-10 11:24:08 +0100

    fixed satelite

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,6 +13,7 @@
         <v-row no gutters>
           <div id="sec_1">
             <v-img src="https://svgshare.com/i/Ggy.svg" height="250" width="250" id="debris_1"></v-img>
+            
             <v-dialog
               v-model="dialog2"
               max-width="300"
@@ -21,8 +22,8 @@
               hide-overlay="true"
             >
               <template v-slot:activator="{ on }">
-                <v-btn icon depressed v-on="on" color="transparent" id="satelite">
-                  <v-img src="https://svgshare.com/i/GeR.svg" height="550" width="550"></v-img>
+                <v-btn icon depressed v-on="on" color="transparent" height="1" width="1" id="satelite" >
+                  <v-img src="https://svgshare.com/i/GeR.svg"  height="550" width="550"></v-img>
                 </v-btn>
               </template>
 
@@ -57,30 +58,26 @@
               v-model="dialog1"
               max-width="300"
               scrollable
-              content-class="dialog1"
+              content-class="dialog"
               hide-overlay="true"
             >
               <template v-slot:activator="{ on }">
-                <v-btn icon depressed v-on="on" color="transparent">
-                  <v-img
-                    src="https://svgshare.com/i/GhY.svg"
-                    height="600"
-                    width="600"
-                    id="satelite2"
-                  ></v-img>
+                <v-btn icon depressed v-on="on" color="transparent" id="satelite2">
+                  <v-img src="https://svgshare.com/i/GhY.svg"  height="550" width="550"></v-img>
                 </v-btn>
               </template>
+
               <v-card>
                 <v-card-title class="headline grey lighten-2" primary-title>Asteroid</v-card-title>
                 <v-card-text>
                   What we see here is a very beautiful Asteroid
-                  Once a upon a time this
+                  Once a upon a time this little thing went like pawww skrrr
                 </v-card-text>
                 <v-divider></v-divider>
                 <v-img
                   id="card-img"
                   contain
-                  src="https://image.flaticon.com/icons/svg/1684/1684370.svg"
+                  src="https://media.giphy.com/media/fYexKcRTSFumjejKPe/giphy.gif"
                   height="170"
                   width="120"
                 ></v-img>
@@ -96,12 +93,109 @@
         </v-row>
 
         <v-row no gutters>
-          <div id="sec_3"></div>
+          <div id="sec_3">
+            <v-dialog
+              v-model="dialog3"
+              max-width="300"
+              scrollable
+              content-class="dialog3"
+              hide-overlay="true"
+            >
+              <template v-slot:activator="{ on }">
+                <v-btn icon depressed v-on="on" color="transparent">
+                  <v-img src="https://svgshare.com/i/GhY.svg" height="600" width="600" id="kamera"></v-img>
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title class="headline grey lighten-2" primary-title>Kamera</v-card-title>
+                <v-card-text>text om astronouten der tabte kameraet i rummet.</v-card-text>
+                <v-divider></v-divider>
+                <v-img
+                  id="card-img"
+                  contain
+                  src="https://image.flaticon.com/icons/svg/1684/1684370.svg"
+                  height="170"
+                  width="120"
+                ></v-img>
+                <v-spacer></v-spacer>
+                <v-divider></v-divider>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="primary" text @click="dialog3 = false">close</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </div>
         </v-row>
 
         <v-row no gutters>
-          <div id="sec_4"></div>
+          <div id="sec_4">
+            <v-dialog
+              v-model="dialog4"
+              max-width="300"
+              scrollable
+              content-class="dialog4"
+              hide-overlay="true"
+            >
+              <template v-slot:activator="{ on }">
+                <v-btn icon depressed v-on="on" color="transparent">
+                  <v-img src="https://svgshare.com/i/GhY.svg" height="600" width="600" id="handske"></v-img>
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title class="headline grey lighten-2" primary-title>Astronaut Handske</v-card-title>
+                <v-card-text>text om astronaut handsken der blev tabt i rummet</v-card-text>
+                <v-divider></v-divider>
+                <v-img
+                  id="card-img"
+                  contain
+                  src="https://image.flaticon.com/icons/svg/1684/1684370.svg"
+                  height="170"
+                  width="120"
+                ></v-img>
+                <v-spacer></v-spacer>
+                <v-divider></v-divider>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="primary" text @click="dialog4 = false">close</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+
+             <v-dialog
+          v-model="dialog5"
+          max-width="300"
+          scrollable
+          content-class="dialog5"
+          hide-overlay="true"
+        >
+          <template v-slot:activator="{ on }">
+            <v-btn icon depressed v-on="on" color="transparent">
+              <v-img src="https://svgshare.com/i/GhY.svg" height="600" width="600" id="netskyder"></v-img>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title class="headline grey lighten-2" primary-title>Netskyder</v-card-title>
+            <v-card-text>tekst om netskyderen</v-card-text>
+            <v-divider></v-divider>
+            <v-img
+              id="card-img"
+              contain
+              src="https://image.flaticon.com/icons/svg/1684/1684370.svg"
+              height="170"
+              width="120"
+            ></v-img>
+            <v-spacer></v-spacer>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" text @click="dialog5 = false">close</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+          </div>
         </v-row>
+       
       </v-container>
       <v-footer color="deep-purple darken-1" padless>
         <v-row justify="center" no-gutters>
@@ -127,8 +221,12 @@
 export default {
   data() {
     return {
-      dialog1: false,
+      dialog1: false, 
       dialog2: false,
+      dialog3: false,
+      dialog4: false,
+      dialog5: false,
+      dialog6: false,
 
       links: ["referencer"]
     };
@@ -176,8 +274,8 @@ export default {
 }
 
 #satelite {
-  position: relative;
-  left: 30%;
+  position: absolute;
+  right: 30%;
   transition: 8s linear;
   -webkit-animation: shake-vertical 10s cubic-bezier(0.455, 0.03, 0.515, 0.955)
     both;
